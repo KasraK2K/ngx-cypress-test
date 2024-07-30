87aef4ec4ec1521169e7c5c4dc579f74b8f6b81b
Kasra Karami <Kasra.Karami.KK@gmail.com>
2024-07-30 14:27:37 +0330

    checkbox

--- cypress/e2e/firstText.spec.js
+++ cypress/e2e/firstText.spec.js
@@ -2,9 +2,11 @@
 
 describe('First test suite', () => {
     beforeEach(() => {
-        cy.visit('/');
-        cy.contains('Forms').click();
-        cy.contains('Form Layouts').click();
+        if (!['Checkboxes'].includes(Cypress.currentTest.title)) {
+            cy.visit('/');
+            cy.contains('Forms').click();
+            cy.contains('Form Layouts').click();
+        }
     });
 
     it('first test', () => {
@@ -26,34 +28,22 @@ describe('First test suite', () => {
     });
 
     it('should save subject of the command', () => {
-        cy.contains('nb-card', 'Using the Grid')
-            .find('[for="inputEmail1"]')
-            .should('contain', 'Email');
-        cy.contains('nb-card', 'Using the Grid')
-            .find('[for="inputPassword2"]')
-            .should('contain', 'Password');
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email');
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password');
 
         // Cypress Alias
         cy.contains('nb-card', 'Using the Grid').as('usingTheGrid');
-        cy.get('@usingTheGrid')
-            .find('[for="inputEmail1"]')
-            .should('contain', 'Email');
-        cy.get('@usingTheGrid')
-            .find('[for="inputPassword2"]')
-            .should('contain', 'Password');
+        cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email');
+        cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password');
 
         // Cypress then() method
         cy.contains('nb-card', 'Using the Grid').then((usingTheGrid) => {
-            cy.wrap(usingTheGrid)
-                .find('[for="inputEmail1"]')
-                .should('contain', 'Email');
-            cy.wrap(usingTheGrid)
-                .find('[for="inputPassword2"]')
-                .should('contain', 'Password');
+            cy.wrap(usingTheGrid).find('[for="inputEmail1"]').should('contain', 'Email');
+            cy.wrap(usingTheGrid).find('[for="inputPassword2"]').should('contain', 'Password');
         });
     });
 
-    it.only('extract text values', () => {
+    it('extract text values', () => {
         // 1
         cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address');
 
@@ -70,9 +60,7 @@ describe('First test suite', () => {
             .then((text) => {
                 expect(text).to.equal('Email address');
             });
-        cy.get('[for="exampleInputEmail1"]')
-            .as('labelText')
-            .should('contain', 'Email address');
+        cy.get('[for="exampleInputEmail1"]').as('labelText').should('contain', 'Email address');
 
         // 4
         cy.get('[for="exampleInputEmail1"]')
@@ -83,8 +71,27 @@ describe('First test suite', () => {
 
         // 5
         cy.get('#exampleInputEmail1').type('test@test.com');
-        cy.get('#exampleInputEmail1')
-            .invoke('prop', 'value')
-            .should('contain', 'test@test.com');
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com');
+    });
+
+    it('Radio buttons', () => {
+        cy.contains('nb-card', 'Using the Grid')
+            .find('[type="radio"]')
+            .then((radioButtons) => {
+                cy.wrap(radioButtons).eq(0).check({ force: true }).should('be.checked');
+                cy.wrap(radioButtons).eq(1).check({ force: true });
+                cy.wrap(radioButtons).eq(0).should('not.be.checked');
+                cy.wrap(radioButtons).eq(2).should('be.disabled');
+            });
+    });
+
+    it.only('Checkboxes', () => {
+        cy.visit('/');
+        cy.contains('Modal & Overlays').click();
+        cy.contains('Toaster').click();
+
+        cy.get('[type="checkbox"]').uncheck({ force: true });
+        cy.get('[type="checkbox"]').eq(1).check({ force: true });
+        cy.get('[type="checkbox"]').eq(0).click({ force: true });
     });
 });
